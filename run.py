8039f4bc2e41918f5f8272b3683cda3fcdd0d04e
siddhijain2008-tech <siddhi.8march@gmail.com>
2026-06-29 23:58:33 +0530

    Update run.py

--- run.py
+++ run.py
@@ -30,6 +30,7 @@ from seed import seed
 init_db()
 seed()
 
+from app import app
 import os
 port = int(os.environ.get("PORT", 5000))
 app.run(debug=False, host="0.0.0.0", port=port, use_reloader=False)
